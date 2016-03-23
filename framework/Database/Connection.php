cd803897174ea4808fd3c82f8a673ffb36125ab6
Lisa Saalfrank <lsaalfrank@hof-university.de>
2016-03-23 03:11:28 +0100

    Wildcat Database class

---
+++ framework/Database/Connection.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Wildcat\Database;
+
+/**
+ * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
+ * @copyright   2016 Lisa Saalfrank
+ * @since       0.1.0
+ * @version     0.1.0
+ * @package     Wildcat
+ * @subpackage  Database
+ */
+class Connection {
+    
+    /**
+     * @var \PDO
+     */
+    private $pdo;
+    
+    /**
+     * @param   array   $config
+     */
+    public function __construct(array $config) {
+        $this->connect($config);
+    }
+    
+    /**
+     * @var  array   $config
+     */
+    private function connect(array $config)
+    {
+        // Build DSN
+        $dns = 'mysql:host='.$config['host'].';dbname='.$config['name'];
+        
+        // Set options
+        $options = array(
+            \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES '.$config['char']
+        );
+        
+        // Create new PDO object
+        $this->pdo = new \PDO($dns, $config['user'], $config['password'], $options);
+    }
+    
+    /**
+     * @param   string      $sql
+     * @param   array|null  $params
+     * @return  \PDOStatement
+     */
+    public function preparedStmt($sql, array $params) 
+    {
+        // Prepare SQL
+        $sql = trim($sql);
+        $stmt = $this->pdo->prepare($sql);
+        
+        // Bind parameters
+        $names = array_keys($params);
+        $elements = count($names);
+        
+        for ($i = 0; $i < $elements; $i++) {
+			$stmt->bindParam(':' . $names[$i], $params[$names[$i]]);
+		}
+        
+        // Execute statement and return
+        $stmt->execute;
+        return $stmt;
+    }
+    
+    /**
+     * @param   string  $sql
+     * @return  \PDOStatement
+     */
+    public function query($sql)
+    {
+        $sql = trim($sql);
+        $stmt = $this->pdo->query($sql);
+        return $stmt;
+    }
+    
+    /**
+     * @param   string  $sql
+     * @return  int
+     */
+    public function execute($sql)
+    {
+        $sql = trim($sql);
+        $rows = $this->pdo->exec($sql);
+        return $rows;
+    }
+}
---
+++ framework/Database/DB.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Wildcat\Database;
+
+use Wildcat\App\Config;
+
+/**
+ * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
+ * @copyright   2016 Lisa Saalfrank
+ * @since       0.1.0
+ * @version     0.1.0
+ * @package     Wildcat
+ * @subpackage  Database
+ */
+class DB {
+    
+    /**
+     * @var \Wildcat\Database\Connection
+     */
+    private static $connection;
+    
+    /**
+     * @param   null|string $name
+     * @return  void
+     */
+    public static function connect($name = null)
+    {
+        if (is_null($name)) {
+            $name = 'default';
+        }
+        
+        if (!is_null(self::connection)) {
+            $config = Config::get['database.' . $name]
+            self::connection = new Connection($config);
+        }
+    }
+    
+    /**
+     * @param   string      $sql
+     * @param   array       $params
+     * @param   null|string $return
+     * @return  void
+     */
+    public static function select($sql, array $params = null) {
+        $result = self::query($sql, $params);
+        return $result->fetchAll(\PDO::FETCH_CLASS, 'stdClass');
+    }
+    
+    /**
+     * @param   string  $sql
+     * @param   array   $params
+     * @return  void
+     */
+    public static function insert($sql, array $params = null) {
+        return self::cud($sql, $params);
+    }
+    
+    /**
+     * @param   string      $sql
+     * @param   array       $params
+     * @param   null|string $return
+     * @return  void
+     */
+    public static function update($sql, array $params = null) {
+        return self::cud($sql, $params);
+    }
+    
+    /**
+     * @param   string      $sql
+     * @param   array       $params
+     * @param   null|string $return
+     * @return  void
+     */
+    public static function delete($sql, array $params = null) {
+        return self::cud($sql, $params);
+    }
+    
+    /**
+     * @param   string      $sql
+     * @param   array       $params
+     * @param   null|string $return
+     * @return  void
+     */
+    private function cud($sql, array $params = null) {
+        if (is_null($params)) {
+            return $this->execute($sql);
+        }
+        return $this->query($sql, $params)->rowCount();
+    }
+    
+    /**
+     * @param   string      $sql
+     * @param   null|array  $params
+     * @return  \PDOStatement
+     */
+    private function query($sql, array $params = null)
+    {
+        if (is_null($params)) {
+            return self::connection->query($sql);
+        }
+        return self::connection->preparedStmt($sql, $params);
+    }
+    
+    /**
+     * @param   string  $sql
+     * @return  int
+     */
+    private function execute($sql) {
+        return self::connection->execute($sql);
+    }
+    
+    /**
+     * @return  void
+     */
+    public static function disconnect() {
+        self::connection = null;
+    }
+}
